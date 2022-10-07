64acf11360f19ae50fe4ccfddae0168fb638bd32
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-10-07 11:19:41 +0100

    feat: change to local database for testing

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product (models.Model):
+    title = models.CharField(max_length=100)
+    discription = models.TextField(blank=True, null=True)
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from shop_app.models import Product
+from .models import Product
 # Create your views here.
 
 
 def home(request):
 
-    return render(request, 'shop_app/index.html')
+    context = {
+        'posts': Product.objects.all()
+    }
+    return render(request, 'shop_app/index.html', context)
