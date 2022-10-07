b84459a183abd7604a6619f10bed80ddfa01aeae
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-10-07 09:19:00 +0100

    feat: add shop app to app settings

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "storages",
+    "shop_app.apps.ShopAppConfig"
     "django.contrib.sites",
     "allauth",
     "allauth.account",
