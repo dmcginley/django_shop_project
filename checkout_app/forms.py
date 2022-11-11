620b4633d6bdad5b0359247e88644a4356259cde
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-11 16:09:53 +0000

    feat: add success page after submitting payment

--- checkout_app/forms.py
+++ checkout_app/forms.py
@@ -6,7 +6,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',  'town_or_city', 'postcode', 'county', 'county')
+                  'street_address1', 'street_address2',  'town_or_city', 'postcode', 'country', 'county')
 
     def __init__(self, *args, **kwargs):
         """
