8c66778967124093cad4e5563545b8b5613a65ef
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-18 08:53:20 +0000

    fix: aws not work can't find css file fix settings

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = ['https://railway.app/','http://127.0.0.1:8000']
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['https://railway.app/', 'http://127.0.0.1:8000']
 
 CSRF_TRUSTED_ORIGINS = ["https://*.railway.app", "https://*.127.0.0.1"]
 
