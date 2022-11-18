31ba754478550cb65a6c0b2feba53fb8e3e36592
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-18 09:59:24 +0000

    chore: change surver around to aws

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['https://web-production-7efa.up.railway.app',
-                 'https://railway.app/', 'http://127.0.0.1:8000']
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ['https://web-production-7efa.up.railway.app',
+#                  'https://railway.app/', 'http://127.0.0.1:8000']
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-7efa.up.railway.app',
                         "https://*.railway.app", "https://*.127.0.0.1"]
