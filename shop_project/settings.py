cedaac57ede1243dc52c7e9f0a1c1e50ecdb9201
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-22 23:48:23 +0000

    chore: add site to allowed hosts

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['https://railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://railway.app/',
+                 'web-production-7efa.up.railway.app', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ["https://*.railway.app", "https://127.0.0.1"]
 
