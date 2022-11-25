3af7b2f8abb3073f45ba672d32aee146972ea5dd
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-25 23:16:51 +0000

    fix: json dump added to cart in checkout app views

--- checkout_app/views.py
+++ checkout_app/views.py
@@ -48,9 +48,12 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(cart)
+            order.save()
             for item_id, item_data in cart.items():
                 try:
                     book = Book.objects.get(id=item_id)
