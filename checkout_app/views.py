9550afceeea6097613b1798700ff3dc3a06966fe
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-27 00:57:23 +0000

    fix: stripe payment intent succeeded error

--- checkout_app/views.py
+++ checkout_app/views.py
@@ -18,9 +18,9 @@ def cache_checkout_data(request):
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'cart': json.dumps(request.session.get('cart', {})),
+            'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
-            'username': request.user
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
