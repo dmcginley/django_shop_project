eaaf5e6c8b1459fe8dddf411634f6bb6f1c2e138
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-27 01:06:22 +0000

    fix: missed a name cart in checkout views

--- checkout_app/views.py
+++ checkout_app/views.py
@@ -18,7 +18,7 @@ def cache_checkout_data(request):
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag', {})),
+            'cart': json.dumps(request.session.get('cart', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
