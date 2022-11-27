0795531571d84723a572eccd5206c20188fe4fee
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-27 15:30:18 +0000

    fix: change product name to book for order line

--- checkout_app/models.py
+++ checkout_app/models.py
@@ -75,7 +75,7 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False,
                               on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(
+    book = models.ForeignKey(
         Book, null=False, blank=False, on_delete=models.CASCADE)
     # product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
@@ -87,8 +87,8 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.book.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'slug {self.product.slug} on order {self.order.order_number}'
+        return f'slug {self.book.slug} on order {self.order.order_number}'
