63ed3d2bc47cd56abc1eddfe63b3395da36ad019
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-27 03:04:42 +0000

    fix: webhook change to older code for api

--- checkout_app/webhook_handler.py
+++ checkout_app/webhook_handler.py
@@ -26,20 +26,32 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+
+# ----------------------------------------------------------------------
+        # intent = event.data.object
+        # pid = intent.id
+        # # print("intent.metadata", intent.metadata)
+        # cart = intent.metadata.cart
+        # save_info = intent.metadata.save_info
+
+        # # Get the Charge object
+        # stripe_charge = stripe.Charge.retrieve(
+        #     intent.latest_charge
+        # )
+
+        # billing_details = stripe_charge.billing_details  # updated
+        # shipping_details = intent.shipping
+        # grand_total = round(stripe_charge.amount / 100, 2)  # updated
+# ----------------------------------------------------------------------
+
         intent = event.data.object
         pid = intent.id
-        # print("intent.metadata", intent.metadata)
-        cart = intent.metadata.cart
+        bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(
-            intent.latest_charge
-        )
-
-        billing_details = stripe_charge.billing_details  # updated
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)  # updated
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
