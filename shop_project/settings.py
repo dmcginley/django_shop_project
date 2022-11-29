c7bbcd49645f555d01dc0dbc943eb58fe41e2569
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-11-29 02:47:02 +0000

    feat: add email functionality to site

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -113,8 +113,6 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
@@ -197,8 +195,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
-# SITE_ID = 1
-SITE_ID = 2
+SITE_ID = 1
+# SITE_ID = 2
 
 
 # Default primary key field type
@@ -228,3 +226,15 @@ STRIPE_CURRENCY = "eur"
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FORM_EMAIL = "bookshop@example.com"
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
--- static/shop_app/js/stripe_elements.js
+++ static/shop_app/js/stripe_elements.js
@@ -76,8 +76,8 @@ form.addEventListener('submit', function (ev) {
                 card: card,
                 billing_details: {
                     name: $.trim(form.full_name.value),
-                    phone: $.trim(form.phone_number.value),
                     email: $.trim(form.email.value),
+                    phone: $.trim(form.phone_number.value),
                     address: {
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
