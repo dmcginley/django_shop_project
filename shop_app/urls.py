0b7a5c8cd10585956f24a0001ad3c5f1bb0cd815
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-12-09 20:03:25 +0000

    fix: search by full author names

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
 from . import views
-from .views import BookDetailView
+from .views import BookDetailView, BookListView
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    path('', BookListView.as_view(), name='home'),
+    # path('search/', views.book_list, name='book_list'),
+
     path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('add/', views.add_book, name='add_book'),
     path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
     path('search/<slug:genre_slug>/', views.genre_list, name='genre_list'),
+    path('book_search/', views.book_search, name='book_search'),
+    path('book_order_view/', views.book_order_view, name='book_order_view'),
+
+
+
+    # path('book_search/', BookSearchView.as_view(), name='book_search'),
 
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -3,98 +3,235 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
-
+# from django.core.paginator import Paginator
 from django.db.models import Q
+import django_filters
+from django.db.models import Value as V
+from django.db.models.functions import Concat
+
+
 from django.views.generic import ListView, DetailView
 
 from .models import Book, Author, Genre
 from .forms import BookForm
 
 
-def home(request):
+# from django.db.models import Q
 
-    books = Book.objects.all()
-    authors = Author.objects.all()
+# def home(request):
 
-    query = None
-    sort = None
-    direction = None
-    # TODO: Queries and Categories, one of two ways
+#     books = Book.objects.all()
+#     authors = Author.objects.all()
 
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'title':
-                sortkey = 'lower_title'
-                # TODO: error lower not defined
-                books = books.annotate(lower_title=Lower('title'))
+#     query = None
+#     sort = None
+#     direction = None
+#     # TODO: Queries and Categories, one of two ways
 
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            books = books.order_by(sortkey)
+#     if request.GET:
+#         if 'sort' in request.GET:
+#             sortkey = request.GET['sort']
+#             sort = sortkey
+#             if sortkey == 'title':
+#                 sortkey = 'lower_title'
+#                 # TODO: error lower not defined
+#                 books = books.annotate(lower_title=Lower('title'))
 
-        if 'genre' in request.GET:
-            genre = request.GET['genre'].split(',')
-            books = books.filter(genre__name__in=genre)
-            genre = Genre.objects.filter(genre__name__in=genre)
+#             if 'direction' in request.GET:
+#                 direction = request.GET['direction']
+#                 if direction == 'desc':
+#                     sortkey = f'-{sortkey}'
+#             books = books.order_by(sortkey)
 
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "Hi, you didn't enter a search")
-                return redirect(reverse('home'))
-            queries = Q(title__icontains=query) | Q(
-                isbn__icontains=query)
-            books = books.filter(queries)
+#         if 'genre' in request.GET:
+#             genre = request.GET['genre'].split(',')
+#             books = books.filter(genre__name__in=genre)
+#             genre = Genre.objects.filter(genre__name__in=genre)
 
-            q2 = Q(first_name__icontains=query) | Q(last_name__icontains=query)
-            authors = authors.filter(q2)
-            # books = books.filter(q2)
+#         if 'q' in request.GET:
+#             query = request.GET['q']
+#             if not query:
+#                 messages.error(request, "Hi, you didn't enter a search")
+#                 return redirect(reverse('home'))
+#             queries = Q(title__icontains=query) | Q(
+#                 isbn__icontains=query)
+#             books = books.filter(queries)
 
-            print(q2)
+#             q2 = Q(first_name__icontains=query) | Q(last_name__icontains=query)
+#             authors = authors.filter(q2)
+#             # books = books.filter(q2)
 
-            # queries = Q(title__icontains=query) | Q(authors__first_name=query)
-            # # books = books.filter(queries)
-            # print('queries=', queries)
+#             print(q2)
 
-            # for a in authors:
-            #     print("found author>> ", str(a))
+#             # queries = Q(title__icontains=query) | Q(authors__first_name=query)
+#             # # books = books.filter(queries)
+#             # print('queries=', queries)
 
-            # a = Author.objects.get(queries)
-            # b = a.book_set.all()
-            # print(b)
-            # print("Q2", q2)
+#             # for a in authors:
+#             #     print("found author>> ", str(a))
 
-            # TODO: find how Django does many-to-many search
-            #  add matching books with found authors to list 'books'
-    else:
-        print("can't find author>> ")
+#             # a = Author.objects.get(queries)
+#             # b = a.book_set.all()
+#             # print(b)
+#             # print("Q2", q2)
 
-    current_sorting = f'{sort}_{direction}'
+#             # TODO: find how Django does many-to-many search
+#             #  add matching books with found authors to list 'books'
+#     else:
+#         print("can't find author>> ")
 
-    context = {
-        # 'books': Book.objects.all(),
-        'books': books,
-        'search_term': query,
-        # 'current_genres': genre,
-        'current_sorting': current_sorting
-    }
+#     current_sorting = f'{sort}_{direction}'
 
-    return render(request, 'shop_app/index.html', context)
+#     context = {
+#         # 'books': Book.objects.all(),
+#         'books': books,
+#         'search_term': query,
+#         # 'current_genres': genre,
+#         'current_sorting': current_sorting
+#     }
 
+#     return render(request, 'shop_app/index.html', context)
+
+##############################################################
 
 class BookListView(ListView):
+    """ class view to list book with pagination """
+    paginate_by = 16
     model = Book
-    # template_name = 'books/index.html'
 
+    template_name = 'shop_app/index.html'
     context_object_name = 'books'
+
+
+def book_search(request):
+    """ GET method search view for authors names, isbn, and title """
+    searching = request.GET.get('searching')
+
+    unique_book_isbns = []
+    results = []
+
+    def add_new_books(book_list):
+        for book in book_list:
+            if book.isbn not in unique_book_isbns:
+                results.append(book)
+                unique_book_isbns.append(book.isbn)
+
+    title_results = []
+    if len(searching) > 1:
+        title_results = Book.objects.filter(title__icontains=searching)
+    add_new_books(title_results)
+
+    isbn_results = []
+    if len(searching) >= 9:
+        # isbn 10 and isbn 13, must have at least 9 numbers
+        isbn_results = Book.objects.filter(isbn__icontains=searching)
+    add_new_books(isbn_results)
+
+    for term in searching.split():
+        matching_authors = []
+        if len(term) > 1:
+            matching_authors = Book.objects.filter(Q(
+                authors__first_name__icontains=term) |
+                Q(authors__last_name__icontains=term))
+        print(f"for {term} added {len(matching_authors)} books")
+        add_new_books(matching_authors)
+        print(f"total books {len(results)}")
+
+    context = {
+        'searching': searching,
+        'results': results,
+        'result_count': len(results)
+    }
+
+    if len(searching.strip()) == 0:
+        messages.error(request, "Please enter a search term")
+    return render(request,
+                  'shop_app/book_search.html', context)
+
+# TODO: fix order by price
+
+
+def book_order_view(request):
+    books = Book.objects.all().order_by('price').values()
+    authors = Author.objects.all()
+
+    context = {
+        'books': books,
+        'authors': authors
+    }
+    return render(request,
+                  'shop_app/index.html', context)
+
+##############################################################
+
+# --------------------------------------------------
+
+
+# def book_list(request):
+#     books = Book.objects.all()
+#     authors = Author.objects.all()
+
+#     query = None
+#     sort = None
+#     direction = None
+#     # TODO: Queries and Categories, one of two ways
+
+#     if request.GET:
+#         if 'sort' in request.GET:
+#             sortkey = request.GET['sort']
+#             sort = sortkey
+#             if sortkey == 'title':
+#                 sortkey = 'lower_title'
+#                 # TODO: error lower not defined
+#                 books = books.annotate(lower_title=Lower('title'))
+
+#             if 'direction' in request.GET:
+#                 direction = request.GET['direction']
+#                 if direction == 'desc':
+#                     sortkey = f'-{sortkey}'
+#             books = books.order_by(sortkey)
+
+#         if 'genre' in request.GET:
+#             genre = request.GET['genre'].split(',')
+#             books = books.filter(genre__name__in=genre)
+#             genre = Genre.objects.filter(genre__name__in=genre)
+
+#         if 'q' in request.GET:
+#             query = request.GET['q']
+#             if not query:
+#                 messages.error(request, "Hi, you didn't enter a search")
+#                 return redirect(reverse('home'))
+#             queries = Q(title__icontains=query) | Q(
+#                 isbn__icontains=query)
+#             books = books.filter(queries)
+
+#             q2 = Q(first_name__icontains=query) | Q(
+#                 last_name__icontains=query)
+#             authors = authors.filter(q2)
+#             # books = books.filter(q2)
+
+#             print(q2)
+
+#     else:
+#         print("can't find author>> ")
+
+#     current_sorting = f'{sort}_{direction}'
+
+#     context = {
+#         # 'books': Book.objects.all(),
+#         'books': books,
+#         'search_term': query,
+#         # 'current_genres': genre,
+#         'current_sorting': current_sorting
+#     }
+
+#     return render(request, 'shop_app/index.html', context)
+
+
+#     context_object_name = 'books'
     # ordering = ['-date_posted']  # date posted in reverse order
-    paginate_by = 4
 
-
+##############################################################
 class BookDetailView(DetailView):
     model = Book
 # TODO: fix book detail maybe using slug
@@ -124,8 +261,10 @@ def genre(request):
         'genres': Genre.objects.all()
     }
 
+##############################################################
 
-@login_required
+
+@ login_required
 def add_book(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
@@ -153,7 +292,7 @@ def add_book(request):
     return render(request, template, context)
 
 
-@login_required
+@ login_required
 def edit_book(request, book_id):
     """ Edit a book in the store """
     if not request.user.is_superuser:
@@ -184,7 +323,7 @@ def edit_book(request, book_id):
     return render(request, template, context)
 
 
-@login_required
+@ login_required
 def delete_book(request, book_id):
     """ Delete a book from the store """
     if not request.user.is_superuser:
