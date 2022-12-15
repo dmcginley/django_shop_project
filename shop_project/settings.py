50eea5319d7ed77dd253c72b8fb62c17887a409c
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-12-15 19:08:09 +0000

    fix: RAILWAY_DEPLOYMENT fix

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG") == "True"
-# RAILWAY_DEPLOYMENT = os.environ.get("RAILWAY_DEPLOYMENT") == "True"
+RAILWAY_DEPLOYMENT = os.environ.get("RAILWAY_DEPLOYMENT") == "True"
 DEBUG = False
 # DEBUG = True
 
