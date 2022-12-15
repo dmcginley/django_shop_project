31119995bfd93df2012b89d6519da702cc024a81
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-12-15 19:28:58 +0000

    fix: requirement file

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -226,10 +226,7 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
 
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-    DEFAULT_FORM_EMAIL = "bookshop@example.com"
-else:
+if RAILWAY_DEPLOYMENT:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
@@ -237,3 +234,7 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+else:
+    print("email dev mode...")
+    EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+    DEFAULT_FORM_EMAIL = "bookshop@example.com"
